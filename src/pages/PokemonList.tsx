998ff18cf0cff710ae513d93145e63068802924b
Bruno Urbano Rodrigues <brunourb@gmail.com>
2024-10-03 20:11:19 -0300

    feat: adicionado datatable customizada

--- src/pages/PokemonList.tsx
+++ src/pages/PokemonList.tsx
@@ -8,14 +8,20 @@ import { Link, useNavigate } from "react-router-dom";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
-        
+import { Paginator } from "primereact/paginator";
+import { IconField } from 'primereact/iconfield';
+import { InputIcon } from 'primereact/inputicon';
+import { InputText } from 'primereact/inputtext';
         
 
 
 const PokemonList: React.FC = () => {
 
-    //criação das props
+    //criação das props = campos(inforamções que você irá receber do usuário)
     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+    const [globalFilter, setGlobalFilter] = useState<string>('');
+    const [first, setFirst] = useState(0);
+    const [rows, setRows] = useState(10);
     const navigate = useNavigate();
 
     //criação de métodos se houver
@@ -37,13 +43,62 @@ const PokemonList: React.FC = () => {
         );
     };
 
+    const paginatorLeft = <Button type="button" icon="pi pi-refresh" text />;
+    const paginatorRight = <Button type="button" icon="pi pi-download" text />;
+
+    //Manipulação de forma explícita do dado contido na datatable
+    //Função de mudança de página
+    const onPageChange = (e: any) => {
+        console.log(e); //componente da tabela
+        setFirst(e.first);
+        setRows(e.rows);
+    }
+
+    //Função para pesquisar na tabela
+    const onGlobalFilterChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        setGlobalFilter(e.target.value);
+    }
+
+    const renderHeader = () => {
+        return (
+            <div className="flex justify-content-end">
+                <IconField iconPosition="left">
+                    <InputIcon className="pi pi-search" />
+                    <InputText value={globalFilter}
+                     onChange={onGlobalFilterChange} 
+                     placeholder="Digite o pokémon" />
+                </IconField>
+            </div>
+        );
+    };
+
+    const header = renderHeader();
+
     //retorno do componente
     return(
         <div>
            <h1>Pokemon List</h1>
            <div className="card">
-            <DataTable value={pokemons} tableStyle={{ minWidth: '50rem' }}>
-                <Column field="name" header="Name" sortable></Column>
+
+
+
+
+            <DataTable value={pokemons}
+                        paginator
+                        rows={5}
+                        rowsPerPageOptions={[5, 10, 25, 50]} 
+                        tableStyle={{ minWidth: '50rem' }}
+                        paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+                        currentPageReportTemplate="{first} to {last} of {totalRecords}"
+                        paginatorLeft={paginatorLeft}
+                        paginatorRight={paginatorRight}
+                        
+                        globalFilter={globalFilter}
+                        globalFilterFields={['name']}
+                        header={header}
+                        emptyMessage="Pokémon não encontrado."
+                >
+                <Column field="name" header="Name" sortable filter></Column>
                 <Column field="Details" body={detailsPokemon}></Column>
             </DataTable>
         </div>
